86b7a74069827e82761c2d07fd6b2b4a9603e9a5
Lawrence Lee <valheru.ashen.shugar@gmail.com>
2024-12-18 11:21:00 +0000

    Added CMake files

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt-get -yq update && sudo apt-get -yq update
+
 cd ~
 curl 'https://invent.kde.org/sdk/kde-builder/-/raw/master/scripts/initial_setup.sh' > initial_setup.sh
 bash initial_setup.sh
